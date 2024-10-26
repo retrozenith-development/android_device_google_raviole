23d07107c74b9dede44d24a14fb065575943836a
Jrcable1 <jrcable1@gmail.com>
2024-10-26 16:20:27 -0400

    raviole: use SCUDO

--- device-lineage.mk
+++ device-lineage.mk
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay-lineage
+PRODUCT_USE_SCUDO := true
 
 # ANGLE - Almost Native Graphics Layer Engine
 PRODUCT_PACKAGES += \
