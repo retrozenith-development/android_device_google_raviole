8dd2ff00fa030e2b826c2067d6f5e8ed50d52676
Pranav Vashi <neobuddy89@gmail.com>
2024-11-11 08:39:36 +0530

    raviole: Enable UDFPS icons and animations
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device-lineage.mk
+++ device-lineage.mk
@@ -17,6 +17,9 @@ $(call inherit-product-if-exists, vendor/google/camera/config.mk)
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
+# Fingerprint
+TARGET_HAS_UDFPS := true
+
 # PowerShare
 include hardware/google/pixel/powershare/device.mk
 
