44c73edd92deb638d68ccf75c44f209ded1dfbc1
Michael Bestas <mkbestas@lineageos.org>
2025-01-13 16:14:21 +0200

    Add missing IWLAN packages
    Change-Id: I671e4f057eb3fbe94f63dc16bf697867278e3eb2

--- device-lineage.mk
+++ device-lineage.mk
@@ -14,6 +14,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
+# IWLAN
+PRODUCT_PACKAGES += \
+    Iwlan
+
 # PowerShare
 include hardware/google/pixel/powershare/device.mk
 
